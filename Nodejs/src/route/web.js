68c38b53d3dac5aad614b4eef842264f2da122ac
Nhật Lưu <vanluunhat.migitech@gmail.com>
2023-04-27 11:30:33 +0700

    27/4/2023

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -10,9 +10,11 @@ let initWebRouters = (app) => {
     router.get('/crud', homeController.getCRUD);
     router.post('/post-crud', homeController.postCRUD);
 
-    router.get('/hoidanit', (req, res) => {
-        return res.send('Hello world with hoidanit');
-    });
+    router.get('/get-crud', homeController.displayGetCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+
+    router.post('/put-crud', homeController.putCRUD);
+    router.get('/delete-crud', homeController.deleteCRUD);
 
     return app.use("/", router);
 }
