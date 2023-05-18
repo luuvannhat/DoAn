c1bfd0ebbabbf339b92b02f3d28cf7a640ed91b4
Nhật Lưu <vanluunhat.migitech@gmail.com>
2023-05-19 00:39:31 +0700

    19/5/2023 video 72

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -11,7 +11,6 @@ let initWebRouters = (app) => {
     router.get('/about', homeController.getAboutPage);
     router.get('/crud', homeController.getCRUD);
     router.post('/post-crud', homeController.postCRUD);
-
     router.get('/get-crud', homeController.displayGetCRUD);
     router.get('/edit-crud', homeController.getEditCRUD);
     router.post('/put-crud', homeController.putCRUD);
@@ -22,10 +21,15 @@ let initWebRouters = (app) => {
     router.post('/api/create-new-user', userController.handleCreateNewUser);
     router.put('/api/edit-user', userController.handleEditUser);
     router.delete('/api/delete-user', userController.handleDeleteUser);
-
-
     router.get('/api/allcode', userController.getAllCode);
+
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
+    router.get('/api/get-all-doctors', doctorController.getAllDoctors);
+    router.post('/api/save-infor-doctors', doctorController.postInforDoctor);
+    router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
+
+
+
 
     return app.use("/", router);
 }
